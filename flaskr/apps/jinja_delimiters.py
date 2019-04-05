d6054c4056b75281f4721990583da297c1a592f9
Ellis Lee <ellis.lee.work@gmail.com>
2019-04-06 01:26:14 +0800

    add jQuery and bootstrap files into project

---
+++ flaskr/apps/jinja_delimiters.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import os
+
+# 初始化Flash物件
+app = Flask(__name__)
+
+
+@app.route('/phone/<int:price>')
+def comment_price(price):
+    return render_template("phone.html", price=price)
+
+
+@app.route('/foreach/list')
+def show_dict():
+    dict = {'name': 'Jack', 'age': 50, 'height': 180, 'weight': 80}
+    return render_template("show_dict.html", dict=dict)
+
+
+@app.route('/foreach/dict')
+def show_dict():
+    dict = {'name': 'Jack', 'age': 50, 'height': 180, 'weight': 80}
+    return render_template("show_dict.html", dict=dict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
